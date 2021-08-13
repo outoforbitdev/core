fd5f6009a654f552c7a1a8ecc118f04dbc251664
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2021-08-13 06:28:56 -0500

    Create serializable dictionary

---
+++ Core/OOD.Core.Collections/SerializableDictionary.cs
@@ -0,0 +1,103 @@
+﻿using OOD.Core.Serializable;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace OOD.Core.Collections
+{
+    public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ICSVSerializable , IXMLSerializable
+        where TKey: ICSVSerializable, IXMLSerializable, new()
+        where TValue: ICSVSerializable, IXMLSerializable, new()
+    {
+        public void DeserializeFromCSV(TextReader stream)
+        {
+            while (true)
+            {
+                this.Clear();
+                string line = stream.ReadLine();
+                if (line == null)
+                {
+                    break;
+                }
+                deserializeLineFromCSV(line);
+            }
+        }
+
+        public void DeserializeFromCSV(string text)
+        {
+            StringReader stream = new StringReader(text);
+            DeserializeFromCSV(stream);
+        }
+        private void deserializeLineFromCSV(string line)
+        {
+            string[] fields = CSVSerializer.GetFields(line);
+            if (fields.Length < 2)
+            {
+                throw new CSVUnparseableException("Unable to parse as key-value pair: " + line);
+            }
+            TKey key = new TKey();
+            key.DeserializeFromCSV(fields[0]);
+            TValue value = new TValue();
+            value.DeserializeFromCSV(string.Join(",", fields.TakeLast(fields.Length - 1)));
+            if (!TryAdd(key, value))
+            {
+                throw new CSVUnparseableException("Duplicate key: " + key.ToString());
+            }
+        }
+
+        public void SerializeToCSV(TextWriter stream)
+        {
+            foreach (TKey k in Keys)
+            {
+                stream.WriteLine(serializeKeyValuePairToCSV(k));
+            }
+        }
+
+        public string SerializeToCSV()
+        {
+            string result = "";
+            foreach(TKey k in Keys)
+            {
+                result = serializeKeyValuePairToCSV(k);
+            }
+            return result.Trim('\n');
+        }
+
+        private string serializeKeyValuePairToCSV(TKey k)
+        {
+            return k.SerializeToCSV() + "," + this[k].SerializeToCSV();
+        }
+
+        public void SerializeToXML(TextWriter stream)
+        {
+            XmlWriter writer = XmlWriter.Create(stream);
+            WriteXml(writer);
+        }
+
+        public void DeserializeFromXML(TextReader stream)
+        {
+            this.Clear();
+            XmlReader reader = XmlReader.Create(stream);
+            ReadXml(reader);
+        }
+
+        public XmlSchema GetSchema()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Core/OOD.Core.Serializable/IXMLSerializable.cs
+++ Core/OOD.Core.Serializable/IXMLSerializable.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 namespace OOD.Core.Serializable
 {
-    public interface IXMLSerializable
+    public interface IXMLSerializable: System.Xml.Serialization.IXmlSerializable
     {
         public void SerializeToXML(TextWriter stream);
         public void DeserializeFromXML(TextReader stream);
