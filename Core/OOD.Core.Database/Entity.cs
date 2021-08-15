6480667b627d6a3c47381b09e66e51e4f8e43c39
Jay Mirecki <43052836+jaymirecki@users.noreply.github.com>
2021-08-15 09:47:16 -0500

    Create database (#2)
    Create Database project

---
+++ Core/OOD.Core.Database/Entity.cs
@@ -0,0 +1,76 @@
+﻿using OOD.Core.Serializable;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Schema;
+
+namespace OOD.Core.Database
+{
+    public abstract class Entity: IXmlSerializable
+    {
+        public string ID
+        {
+            get { return _id; }
+            set 
+            { 
+                if (ID == null)
+                {
+                    _id = value;
+                } 
+            }
+        }
+        protected string _id;
+        protected string _tag = "Entity";
+        internal Database _db;
+
+        protected abstract void Clear();
+        public void DeserializeFromXml(TextReader stream)
+        {
+            Clear();
+            XmlReader reader = XmlReader.Create(stream);
+            ReadXml(reader, false);
+            reader.Close();
+        }
+
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(XmlReader reader)
+        {
+            ReadXml(reader, false);
+        }
+
+        public abstract void ReadXml(XmlReader reader, bool ignoreItemTag = false);
+        protected static void DeserializeItemFromXml<T>(XmlReader reader, Item<T> item)
+            where T: IEquatable<T>
+        {
+            item.ReadXml(reader, false);
+        }
+
+        public void SerializeToXml(TextWriter stream)
+        {
+            XmlWriter writer = XmlWriter.Create(stream);
+            WriteXml(writer, false);
+            writer.Flush();
+            writer.Close();
+        }
+
+        public abstract void WriteXml(XmlWriter writer, bool ignoreItemTag = false);
+
+        public void WriteXml(XmlWriter writer)
+        {
+            WriteXml(writer, false);
+        }
+        protected static void SerializeItemToXml<T>(XmlWriter writer, Item<T> item)
+            where T : IEquatable<T>
+        {
+            item.WriteXml(writer, false);
+        }
+    }
+}
---
+++ Core/OOD.Core.Database/Item.cs
@@ -0,0 +1,76 @@
+﻿using OOD.Core.Serializable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace OOD.Core.Database
+{
+    public class Item<T> : XmlSerializable, IEquatable<Item<T>>
+        where T: IEquatable<T>
+    {
+        private T _defaultValue;
+        private T _value;
+        private new readonly string _tag;
+        private static readonly System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+        public T Value
+        {
+            get
+            {
+                if (_useDefaultValue)
+                {
+                    return _defaultValue;
+                }
+                return _value;
+            }
+            set { 
+                _value = value;
+                _useDefaultValue = false;
+            }
+        }
+        public bool UsingDefaultValue { get { return _useDefaultValue; } }
+        private bool _useDefaultValue;
+        public Item(string tag, T defaultValue)
+        {
+            _tag = tag;
+            _defaultValue = defaultValue;
+            _useDefaultValue = true;
+        }
+        public override void ReadXml(XmlReader reader, bool ignoreItemTag = false)
+        {
+            try
+            {
+                _useDefaultValue = !DeserializeProperty(reader, serializer, _tag, out _value, true);
+            }
+            finally
+            {
+                
+            }
+        }
+
+        public override void WriteXml(XmlWriter writer, bool ignoreItemTag = false)
+        {
+            if (!UsingDefaultValue)
+            {
+                SerializeProperty(writer, serializer, _tag, _value, true);
+            }
+        }
+
+        public bool Equals(Item<T> other)
+        {
+            return UsingDefaultValue == other.UsingDefaultValue && Value.Equals(other.Value);
+        }
+
+        public static bool operator ==(Item<T> a, Item<T> b)
+        {
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Item<T> a, Item<T> b)
+        {
+            return !(a == b);
+        }
+    }
+}
